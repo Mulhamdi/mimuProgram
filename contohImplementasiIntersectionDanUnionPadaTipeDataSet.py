2fd5bb93e416b0f9dd339c576d1668337217b40a
Mulhamdi <77982506+Mulhamdi@users.noreply.github.com>
2022-06-23 15:19:56 +0700

    Create contohImplementasiIntersectionDanUnionPadaTipeDataSet.py
    Itung-itung menuhin repository aja hehe.

---
+++ contohImplementasiIntersectionDanUnionPadaTipeDataSet.py
@@ -0,0 +1,11 @@
+# Intersection/Irisan
+a = set([1, 2, 3])
+b = set([2, 3, 4])
+hasil = a.intersection(b)
+print('Intersection:', hasil)
+
+# Union/Gabungan
+a = set([1, 2, 3])
+b = set([2, 3, 4])
+hasil = a.union(b)
+print('Union:', hasil)
