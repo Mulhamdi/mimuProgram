38ac80d598289ad24cf8f00b510186923c49c512
Mulhamdi <77982506+Mulhamdi@users.noreply.github.com>
2022-06-23 14:31:01 +0700

    Create contohOrderedDanUnordered.py
    Kode ini dibuat sebagai contoh output dari konsep koleksi yang bersifat ordered dan unordered pada Python, dapat disimpulkan bahwa output variabel Set (unordered collection) bersifat acak atau tidak terurut. Sedangkan untuk varibel String, List dan Tuple output-nya bersifat terurut (ordered collecton).

---
+++ contohOrderedDanUnordered.py
@@ -0,0 +1,14 @@
+abjad = 'abcdefghijklmnopqrstuvwxyzABCDEVGHIJKLMNOPQRSTUVWXYZ'
+
+abjad_string = str(abjad)
+abjad_list = list(abjad)
+abjad_tuple = tuple(abjad)
+abjad_set = set(abjad)
+
+print('String: ', abjad_string)
+print()  # Untuk kemudahan dalam membaca output
+print('Lists: ', abjad_list)
+print()  # Untuk kemudahan dalam membaca output
+print('Tuples: ', abjad_tuple)
+print()  # Untuk kemudahan dalam membaca output
+print('Sets: ', abjad_set)
